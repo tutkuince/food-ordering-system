3bd1e9e93e5eb9902d1736bd60011fa0af92f210
Tutku Ince <tutku.ince@outlook.com>
2023-12-03 21:35:34 +0300

    update

--- food-ordering-system/order-service/order-dataaccess/src/main/java/com/food/ordering/system/order/server/dataaccess/order/mapper/OrderDataAccessMapper.java
+++ food-ordering-system/order-service/order-dataaccess/src/main/java/com/food/ordering/system/order/server/dataaccess/order/mapper/OrderDataAccessMapper.java
@@ -1,15 +1,23 @@
 package com.food.ordering.system.order.server.dataaccess.order.mapper;
 
+import com.food.ordering.system.domain.valueobject.*;
 import com.food.ordering.system.order.server.dataaccess.order.entity.OrderAddressEntity;
 import com.food.ordering.system.order.server.dataaccess.order.entity.OrderEntity;
 import com.food.ordering.system.order.server.dataaccess.order.entity.OrderItemEntity;
 import com.food.ordering.system.order.service.domain.entity.Order;
 import com.food.ordering.system.order.service.domain.entity.OrderItem;
+import com.food.ordering.system.order.service.domain.entity.Product;
+import com.food.ordering.system.order.service.domain.valueobject.OrderItemId;
 import com.food.ordering.system.order.service.domain.valueobject.StreetAddress;
+import com.food.ordering.system.order.service.domain.valueobject.TrackingId;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import static com.food.ordering.system.order.service.domain.entity.Order.FAILURE_MESSAGE_DELIMITER;
 
 @Component
 public class OrderDataAccessMapper {
@@ -24,11 +32,45 @@ public class OrderDataAccessMapper {
                 .items(orderItemsToOrderItemEntities(order.getItems()))
                 .orderStatus(order.getOrderStatus())
                 .failureMessages(order.getFailureMessages() != null ?
-                        String.join(Order.FAILURE_MESSAGE_DELIMITER, order.getFailureMessages()) : "")
+                        String.join(FAILURE_MESSAGE_DELIMITER, order.getFailureMessages()) : "")
                 .build();
         orderEntity.getAddress().setOrder(orderEntity);
         orderEntity.getItems().forEach(orderItemEntity -> orderItemEntity.setOrder(orderEntity));
         return orderEntity;
+    }
+
+    public Order orderEntityToOrder(OrderEntity orderEntity) {
+        return Order.Builder.builder()
+                .orderId(new OrderId(orderEntity.getId()))
+                .customerId(new CustomerId(orderEntity.getCustomerId()))
+                .restaurantId(new RestaurantId(orderEntity.getRestaurantId()))
+                .deliveryAddress(addressEntityToDeliveryAddress(orderEntity.getAddress()))
+                .price(new Money(orderEntity.getPrice()))
+                .items(orderItemEntitiesToOrderItem(orderEntity.getItems()))
+                .trackingId(new TrackingId(orderEntity.getTrackingId()))
+                .orderStatus(orderEntity.getOrderStatus())
+                .failureMessages(orderEntity.getFailureMessages().isEmpty() ? new ArrayList<>() :
+                        new ArrayList<>(Arrays.asList(orderEntity.getFailureMessages().split(FAILURE_MESSAGE_DELIMITER))))
+                .build();
+    }
+
+    private List<OrderItem> orderItemEntitiesToOrderItem(List<OrderItemEntity> items) {
+        return items.stream()
+                .map(orderItemEntity -> OrderItem.Builder.builder()
+                        .orderItemId(new OrderItemId(orderItemEntity.getId()))
+                        .product(new Product(new ProductId(orderItemEntity.getProductId())))
+                        .price(new Money(orderItemEntity.getPrice()))
+                        .quantity(orderItemEntity.getQuantity())
+                        .subTotal(new Money(orderItemEntity.getSubTotal()))
+                        .build())
+                .collect(Collectors.toList());
+    }
+
+    private StreetAddress addressEntityToDeliveryAddress(OrderAddressEntity address) {
+        return new StreetAddress(address.getId(),
+                address.getStreet(),
+                address.getPostalCode(),
+                address.getCity());
     }
 
     private List<OrderItemEntity> orderItemsToOrderItemEntities(List<OrderItem> items) {
