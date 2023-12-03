80d5653188f013a6d9971bb5e2bdadf890817877
Tutku Ince <tutku.ince@outlook.com>
2023-12-03 21:22:09 +0300

    Order Data Mapper has been created

---
+++ food-ordering-system/order-service/order-dataaccess/src/main/java/com/food/ordering/system/order/server/dataaccess/order/mapper/OrderDataAccessMapper.java
@@ -0,0 +1,54 @@
+package com.food.ordering.system.order.server.dataaccess.order.mapper;
+
+import com.food.ordering.system.order.server.dataaccess.order.entity.OrderAddressEntity;
+import com.food.ordering.system.order.server.dataaccess.order.entity.OrderEntity;
+import com.food.ordering.system.order.server.dataaccess.order.entity.OrderItemEntity;
+import com.food.ordering.system.order.service.domain.entity.Order;
+import com.food.ordering.system.order.service.domain.entity.OrderItem;
+import com.food.ordering.system.order.service.domain.valueobject.StreetAddress;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class OrderDataAccessMapper {
+    public OrderEntity orderToOrderEntity(Order order) {
+        OrderEntity orderEntity = OrderEntity.builder()
+                .id(order.getId().getValue())
+                .customerId(order.getCustomerId().getValue())
+                .restaurantId(order.getRestaurantId().getValue())
+                .trackingId(order.getTrackingId().getValue())
+                .address(deliveryAddressToAddressEntity(order.getDeliveryAddress()))
+                .price(order.getPrice().getAmount())
+                .items(orderItemsToOrderItemEntities(order.getItems()))
+                .orderStatus(order.getOrderStatus())
+                .failureMessages(order.getFailureMessages() != null ?
+                        String.join(Order.FAILURE_MESSAGE_DELIMITER, order.getFailureMessages()) : "")
+                .build();
+        orderEntity.getAddress().setOrder(orderEntity);
+        orderEntity.getItems().forEach(orderItemEntity -> orderItemEntity.setOrder(orderEntity));
+        return orderEntity;
+    }
+
+    private List<OrderItemEntity> orderItemsToOrderItemEntities(List<OrderItem> items) {
+        return items.stream()
+                .map(orderItem -> OrderItemEntity.builder()
+                        .id(orderItem.getId().getValue())
+                        .productId(orderItem.getProduct().getId().getValue())
+                        .price(orderItem.getPrice().getAmount())
+                        .quantity(orderItem.getQuantity())
+                        .subTotal(orderItem.getSubTotal().getAmount())
+                        .build())
+                .collect(Collectors.toList());
+    }
+
+    private OrderAddressEntity deliveryAddressToAddressEntity(StreetAddress deliveryAddress) {
+        return OrderAddressEntity.builder()
+                .id(deliveryAddress.getId())
+                .street(deliveryAddress.getStreet())
+                .postalCode(deliveryAddress.getPostalCode())
+                .city(deliveryAddress.getCity())
+                .build();
+    }
+}
