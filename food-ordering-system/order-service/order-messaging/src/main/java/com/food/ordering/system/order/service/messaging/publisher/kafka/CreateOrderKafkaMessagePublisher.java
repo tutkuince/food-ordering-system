03de2a6dc680cb4f9378174dd1eb4d4dcdd3e9e9
Tutku Ince <tutku.ince@outlook.com>
2023-12-04 22:43:14 +0300

    Publisher

---
+++ food-ordering-system/order-service/order-messaging/src/main/java/com/food/ordering/system/order/service/messaging/publisher/kafka/CreateOrderKafkaMessagePublisher.java
@@ -0,0 +1,65 @@
+package com.food.ordering.system.order.service.messaging.publisher.kafka;
+
+import com.food.ordering.system.kafka.order.avro.model.PaymentRequestAvroModel;
+import com.food.ordering.system.kafka.producer.service.KafkaProducer;
+import com.food.ordering.system.order.service.domain.config.OrderServiceConfigData;
+import com.food.ordering.system.order.service.domain.event.OrderCreatedEvent;
+import com.food.ordering.system.order.service.domain.ports.output.message.publisher.payment.OrderCreatedPaymentRequestMessagePublisher;
+import com.food.ordering.system.order.service.messaging.mapper.OrderMessagingDataMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.stereotype.Component;
+
+import java.util.function.BiConsumer;
+
+@Slf4j
+@Component
+public class CreateOrderKafkaMessagePublisher implements OrderCreatedPaymentRequestMessagePublisher {
+
+    private final OrderMessagingDataMapper orderMessagingDataMapper;
+    private final OrderServiceConfigData orderServiceConfigData;
+    private final KafkaProducer<String, PaymentRequestAvroModel> kafkaProducer;
+
+    public CreateOrderKafkaMessagePublisher(OrderMessagingDataMapper orderMessagingDataMapper,
+                                            OrderServiceConfigData orderServiceConfigData,
+                                            KafkaProducer<String, PaymentRequestAvroModel> kafkaProducer) {
+        this.orderMessagingDataMapper = orderMessagingDataMapper;
+        this.orderServiceConfigData = orderServiceConfigData;
+        this.kafkaProducer = kafkaProducer;
+    }
+
+    @Override
+    public void publish(OrderCreatedEvent domainEvent) {
+        String orderId = domainEvent.getOrder().getId().getValue().toString();
+        log.info("Received OrderCreatedEvent for order id: {}", orderId);
+
+        try {
+            PaymentRequestAvroModel paymentRequestAvroModel = orderMessagingDataMapper
+                    .orderCreatedEventToPaymentRequestAvroModel(domainEvent);
+
+            kafkaProducer.send(orderServiceConfigData.getPaymentRequestTopicName(), orderId, paymentRequestAvroModel,
+                    getKafkaCallback(orderServiceConfigData, paymentRequestAvroModel));
+            log.info("PaymentRequestAvroModel sent to Kafka for order id: {}", paymentRequestAvroModel.getOrderId());
+        } catch (Exception e) {
+            log.error("Error while sending PaymentRequestAvroModel message");
+        }
+    }
+
+    private BiConsumer<SendResult<String, PaymentRequestAvroModel>, Throwable> getKafkaCallback(OrderServiceConfigData orderServiceConfigData,
+                                                                                                PaymentRequestAvroModel paymentRequestAvroModel) {
+        return (result, ex) -> {
+            if (ex == null) {
+                RecordMetadata metadata = result.getRecordMetadata();
+                log.info("Received new metadata. Topic: {}; Partition {}; Offset {}; Timestamp {}, at time {}",
+                        metadata.topic(),
+                        metadata.partition(),
+                        metadata.offset(),
+                        metadata.timestamp(),
+                        System.nanoTime());
+            } else {
+                log.error("Error while sending message {} to topic {}", paymentRequestAvroModel.toString(), orderServiceConfigData.getPaymentRequestTopicName(), ex);
+            }
+        };
+    }
+}
