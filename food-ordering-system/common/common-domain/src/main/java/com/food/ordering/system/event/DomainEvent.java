da602b1cc0859eb9c55c8377e4a2b6f8059f7748
Tutku Ince <tutku.ince@outlook.com>
2023-12-13 22:58:43 +0300

    all modules have been removed

--- food-ordering-system/common/common-domain/src/main/java/com/food/ordering/system/event/DomainEvent.java
+++
@@ -1,4 +0,0 @@
-package com.food.ordering.system.event;
-
-public interface DomainEvent<T> {
-}
--- food-ordering-system/common/common-domain/src/main/java/com/food/ordering/system/event/publisher/DomainEventPublisher.java
+++
@@ -1,7 +0,0 @@
-package com.food.ordering.system.event.publisher;
-
-import com.food.ordering.system.event.DomainEvent;
-
-public interface DomainEventPublisher<T extends DomainEvent> {
-    void publish(T domainEvent);
-}
