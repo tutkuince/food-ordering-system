e66af5fbb0ed389c7d2c5da7b29f8df10d877831
Tutku Ince <tutku.ince@outlook.com>
2024-02-13 22:42:36 +0300

    OrderApplicationService as a input port

---
+++ food-ordering-system/order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/ports/input/service/OrderApplicationService.java
@@ -0,0 +1,11 @@
+package com.food.ordering.system.order.service.domain.ports.input.service;
+
+import com.food.ordering.system.order.service.domain.dto.create.CreateOrderCommand;
+import com.food.ordering.system.order.service.domain.dto.create.CreateOrderResponse;
+import com.food.ordering.system.order.service.domain.dto.track.TrackOrderQuery;
+import com.food.ordering.system.order.service.domain.dto.track.TrackOrderResponse;
+
+public interface OrderApplicationService {
+    CreateOrderResponse createOrder(CreateOrderCommand createOrderCommand);
+    TrackOrderResponse trackOrder(TrackOrderQuery trackOrderQuery);
+}
