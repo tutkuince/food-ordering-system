515d46ae0bfade0d41dd12ee1d9617033452cd4f
Tutku Ince <tutku.ince@outlook.com>
2024-02-14 19:14:01 +0300

    added new constructor

--- food-ordering-system/order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/entity/Product.java
+++ food-ordering-system/order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/entity/Product.java
@@ -4,14 +4,21 @@ import com.food.ordering.system.domain.entity.BaseEntity;
 import com.food.ordering.system.domain.valueobject.Money;
 import com.food.ordering.system.domain.valueobject.ProductId;
 
+import java.util.UUID;
+
 public class Product extends BaseEntity<ProductId> {
     private String name;
     private Money price;
+
 
     public Product(ProductId productId, String name, Money price) {
         super.setId(productId);
         this.name = name;
         this.price = price;
+    }
+
+    public Product(ProductId productId) {
+        super.setId(productId);
     }
 
     public void updateWithConfirmedNamedAndPrice(String name, Money price) {
