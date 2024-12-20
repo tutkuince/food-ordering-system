e577ae6e8e5cbcb38296c0e42d8056577f3378f2
Tutku Ince <tutku.ince@outlook.com>
2024-12-20 18:58:04 +0300

    initial commit

---
+++ src/main/java/com/incetutku/foodorderingsystem/FoodOrderingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.incetutku.foodorderingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodOrderingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodOrderingSystemApplication.class, args);
+    }
+
+}
